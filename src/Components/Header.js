788427156c64d304ef23ed8ff4891f68336bb2df
Nicole Dow <nikki.dow71@gmail.com>
2020-09-01 20:08:16 -0700

    header and nav skeleton

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function header() {
+  return (
+    <div>
+      <h1>Nicole Dow</h1>
+      <h2>Full Stack Developer</h2>
+    </div>
+  );
+}
+
+export default header;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function navbar() {
+    return(
+        <div>
+            <a href="https://medium.com/@ndow_11367" target="_blank">Blog</a>
+            <a href="https://drive.google.com/file/d/1BEDHC5Gb1ZEOUilEmmje1bNeMi5EA1mZ/view">Resume</a>
+        </div>
+    )
+}
+
+export default navbar;
